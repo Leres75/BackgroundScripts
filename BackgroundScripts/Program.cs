224c9e229abc36cacd386a6344cecd61a8c88597
Pavel Keßler <pavel.kessler@gmx.de>
2018-11-27 21:34:40 +0100

    checks every 10 minutes for removable drives and creates a Backup

--- BackgroundScripts/Program.cs
+++ BackgroundScripts/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace BackgroundScripts
 {
     class Program
     {
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+            while (true)
+            {
+                ThumbdriveBackup.Backup();
+                Thread.Sleep(600000);
+            }
         }
     }
 }
---
+++ BackgroundScripts/ThumbdriveBackup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace BackgroundScripts
+{
+    class ThumbdriveBackup
+    {
+        public static void Backup()
+        {
+            var drives = DriveInfo.GetDrives().Where(drive => drive.DriveType.Equals(DriveType.Removable));
+            DirectoryInfo destDirectory;
+            foreach (DriveInfo d in drives)
+            {
+                destDirectory = new DirectoryInfo(@"D:\USBBackups\" + d.VolumeLabel + @"\");
+                DirectoryInfo sourceDirectory = d.RootDirectory;
+                CopyFolder(sourceDirectory, destDirectory);
+            }
+        }
+
+        public static void CopyFolder(DirectoryInfo source, DirectoryInfo target)
+        {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+            {
+                CopyFolder(dir, target.CreateSubdirectory(dir.Name));
+            }
+
+            foreach (FileInfo file in source.GetFiles())
+            {
+                if(File.Exists(Path.Combine(target.FullName, file.Name) )){
+                    FileInfo targetFile = new FileInfo(Path.Combine(target.FullName, file.Name));
+                    if(file.LastWriteTime != targetFile.LastWriteTime)
+                    {
+                        file.CopyTo(Path.Combine(target.FullName, file.Name), true);
+                        targetFile.LastWriteTime = file.LastWriteTime;
+                    }
+                }
+                else
+                {
+                    file.CopyTo(Path.Combine(target.FullName, file.Name));
+                    FileInfo targetFile = new FileInfo(Path.Combine(target.FullName, file.Name));
+                    try{
+                        targetFile.LastWriteTime = file.LastWriteTime;
+                    }catch(UnauthorizedAccessException e)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
